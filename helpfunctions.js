ce722f259e17ad35d2888756848b6157df07273c
ranabhat <paribessh_sab@hotmail.com>
2019-09-17 11:07:13 +0300

    function to find out text length, word count from a given text

--- helpfunctions.js
+++ helpfunctions.js
@@ -1,18 +1,16 @@
 module.exports = {
-    analyze: (receiveValue) => {
-        const sentenceBody = receiveValue // Object Property Text Length
-        const textWithReplacedSpace = sentenceBody.replace(/\s/g, '') // All spaces stripped from the string
-        const textWithRemovedNumber = textWithReplacedSpace.replace(/[^a-zA-Z ]/g, '')
-        const alphabeticOrder = textWithRemovedNumber.split('').sort().join('')
-        const wordsInSentence = sentenceBody.trim().split(/\s+/).length // words counts
-        const counter = str => {
-          return str.split('').reduce((total, letter) => {
-            total[letter] ? total[letter]++ : total[letter] = 1
-            return total
-          }, {})
-        }
-        return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, [counter(alphabeticOrder)]]
-      }
+  analyze: (receiveValue) => {
+    const sentenceBody = receiveValue // Object Property Text Length
+    const textWithReplacedSpace = sentenceBody.replace(/\s/g, '') // All spaces stripped from the string
+    const textWithRemovedNumber = textWithReplacedSpace.replace(/[^a-zA-Z ]/g, '')
+    const alphabeticOrder = textWithRemovedNumber.split('').sort().join('')
+    const wordsInSentence = sentenceBody.trim().split(/\s+/).length // words counts
+    const counter = str => {
+      return str.split('').reduce((total, letter) => {
+        total[letter] ? total[letter]++ : total[letter] = 1
+        return total
+      }, {})
+    }
+    return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, [counter(alphabeticOrder)]]
+  }
 }
-
-
