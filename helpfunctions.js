ebf6bd76bd9c8e16ee411abdfa9fda2cc458dc9a
ranabhat <paribessh_sab@hotmail.com>
2019-09-17 23:19:29 +0300

    add info text for analyze function

--- helpfunctions.js
+++ helpfunctions.js
@@ -1,29 +1,36 @@
 module.exports = {
   analyze: (receiveValue) => {
     const sentenceBody = receiveValue // Object Property Text Length
+
     const textWithReplacedSpace = sentenceBody.replace(/\s/g, '') // All spaces stripped from the string
     const textWithRemovedNumber = textWithReplacedSpace.replace(/[^a-zA-Z ]/g, '')
+
     const textSplit = textWithRemovedNumber.split('')
+
     // return all values from array in lowercase
     const alphabetLowercase = textSplit.map((value) => {
       return value.toLowerCase()
     })
     const alphabeticOrder = alphabetLowercase.sort().join('')
     const wordsInSentence = (sentenceBody === '') ? 0 : sentenceBody.trim().split(/\s+/).length // words counts
+
+    // counter = (‘abc’) => (Object { a: 1, b: 1, c: 1 })
     const counter = str => {
       return str.split('').reduce((total, letter) => {
         total[letter] ? total[letter]++ : total[letter] = 1
         return total
       }, {})
     }
+
     const getAlphabetCount = counter(alphabeticOrder)
-    const arrayOfEachAlphabetCount = Object.entries(getAlphabetCount).reduce((o, [k, v]) => {
+    // (Object { a: 1, b: 1, c: 1 } => Object {“a”: {“a”: 3 }, “b”: { “b”: 1},”c”: { “c”: 1)}
+    const arrayOfEachAlphabetCount = Object.entries(getAlphabetCount).reduce((o, [k, v]) => { // k- key, v- value
       const [letter] = k.match(/\D+|\d+$/g)
       o[letter] = { ...(o[letter] || {}), [letter]: v }
       return o
     }, {})
 
-    const arrayOfObjectAlbhabet = Object.values(arrayOfEachAlphabetCount)
+    const arrayOfObjectAlbhabet = Object.values(arrayOfEachAlphabetCount) // = > Array [Object { a: 1 }, Object { b: 2 }, Object { c: 3 }]
     return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, arrayOfObjectAlbhabet]
   }
 }
