e59da881d81d6fd9fc3ce9e45cf8ae5add8eae4b
ranabhat <paribessh_sab@hotmail.com>
2019-09-17 20:58:28 +0300

    test to be done

--- test/analyze.test.js
+++ test/analyze.test.js
@@ -11,3 +11,11 @@ test('from provided text calculate array of[ text length , word count, character
 test('from provided text calculate array of[ text length , word count, character count -test3]', () => {
   expect(analyze.analyze('hello 2 times  ')).toEqual([15, 11, 3, [{ e: 2 }, { h: 1 }, { i: 1 }, { l: 2 }, { m: 1 }, { o: 1 }, { s: 1 }, { t: 1 }]])
 })
+
+test('when empty text provide -test4', () => {
+  expect(analyze.analyze('')).toEqual([0, 0, 0, []])
+})
+
+test('when capital letters provided -test5', () => {
+  expect(analyze.analyze('eHllo')).toEqual([5, 5, 1, [{ e: 1 }, { h: 1 }, { l: 2 }, { o: 1 }]])
+})
