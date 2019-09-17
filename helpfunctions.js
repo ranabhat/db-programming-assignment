ca9c72b24ea75f1d7e301cbb4ce98c4ad8b00f4e
ranabhat <paribessh_sab@hotmail.com>
2019-09-17 14:01:19 +0300

    edit helper function and package json

--- helpfunctions.js
+++ helpfunctions.js
@@ -8,9 +8,29 @@ module.exports = {
     const counter = str => {
       return str.split('').reduce((total, letter) => {
         total[letter] ? total[letter]++ : total[letter] = 1
+        // console.log(total)
+        // console.log(total[letter])
         return total
       }, {})
     }
+    const test1 = (counter(alphabeticOrder))
+    console.log(typeof(test1))
+    console.log(test1['e'])
+    const items = Object.keys(test1).map(function(key) {
+       item1 = `{"${key}" : ${test1[key]}}`
+       replaceItem1 = item1.replace(/'/g, '')
+       return item1
+    })
+    console.log(items)
+    const diii = []
+    for (const key of Object.keys(test1)) {
+      
+      const value = test1[key]
+     // console.log(`{'${key}' : ${value}}`)
+      diii.push(`{"${key}" : ${value}}`)
+      
+   }
+    console.log(diii)
     return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, [counter(alphabeticOrder)]]
   }
 }
