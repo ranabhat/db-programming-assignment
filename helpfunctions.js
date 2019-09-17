1e5ba4c96881de0d715f9f5d10050aa4c06cf65a
ranabhat <paribessh_sab@hotmail.com>
2019-09-17 19:29:50 +0300

    gitve an array of object of alphabet count

--- helpfunctions.js
+++ helpfunctions.js
@@ -8,29 +8,17 @@ module.exports = {
     const counter = str => {
       return str.split('').reduce((total, letter) => {
         total[letter] ? total[letter]++ : total[letter] = 1
-        // console.log(total)
-        // console.log(total[letter])
         return total
       }, {})
     }
-    const test1 = (counter(alphabeticOrder))
-    console.log(typeof(test1))
-    console.log(test1['e'])
-    const items = Object.keys(test1).map(function(key) {
-       item1 = `{"${key}" : ${test1[key]}}`
-       replaceItem1 = item1.replace(/'/g, '')
-       return item1
-    })
-    console.log(items)
-    const diii = []
-    for (const key of Object.keys(test1)) {
-      
-      const value = test1[key]
-     // console.log(`{'${key}' : ${value}}`)
-      diii.push(`{"${key}" : ${value}}`)
-      
-   }
-    console.log(diii)
-    return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, [counter(alphabeticOrder)]]
+    const getAlphabetCount = counter(alphabeticOrder)
+    const arrayOfEachAlphabetCount = Object.entries(getAlphabetCount).reduce((o, [k, v]) => {
+      const [letter] = k.match(/\D+|\d+$/g)
+      o[letter] = { ...(o[letter] || {}), [letter]: v }
+      return o
+    }, {})
+
+    const arrayOfObjectAlbhabet = Object.values(arrayOfEachAlphabetCount)
+    return [sentenceBody.length, textWithReplacedSpace.length, wordsInSentence, arrayOfObjectAlbhabet]
   }
 }
